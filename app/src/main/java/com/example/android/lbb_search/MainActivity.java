75879be53c94d067194be7cbd23ab23b2ab54881
krohit97 <lifecoderohit@gmail.com>
2017-04-12 23:19:33 +0530

    Google Map added

--- app/src/main/java/com/example/android/lbb_search/MainActivity.java
+++ app/src/main/java/com/example/android/lbb_search/MainActivity.java
@@ -1,19 +1,35 @@
 package com.example.android.lbb_search;
 
+import android.Manifest;
+import android.app.Dialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlaceAutocomplete;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
 public class MainActivity extends AppCompatActivity {
+
+    GoogleMap googleMap;
 
 
     @Override
@@ -21,7 +37,57 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getBaseContext());
+        if (status != ConnectionResult.SUCCESS) { // Google Play Services are not available
+
+            int requestCode = 10;
+            Dialog dialog = GooglePlayServicesUtil.getErrorDialog(status, this, requestCode);
+            dialog.show();
+
+        } else { // Google Play Services are available
+
+            // Getting reference to the SupportMapFragment of activity_main.xml
+            SupportMapFragment fm = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+
+            // Getting GoogleMap object from the fragment
+            googleMap = fm.getMap();
+
+            // Enabling MyLocation Layer of Google Map
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            googleMap.setMyLocationEnabled(true);
+
+            // Getting LocationManager object from System Service LOCATION_SERVICE
+            LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+            // Creating a criteria object to retrieve provider
+            Criteria criteria = new Criteria();
+
+            // Getting the name of the best provider
+            String provider = locationManager.getBestProvider(criteria, true);
+
+            // Getting Current Location
+            Location location = locationManager.getLastKnownLocation(provider);
+
+            if(location!=null){
+                onLocationChanged(location);
+            }
+
+            locationManager.requestLocationUpdates(provider, 20000, 0, (LocationListener) this);
+        }
+
+
     }
+
+
 
 
     public void findPlace(View view) {
@@ -44,19 +110,19 @@ public class MainActivity extends AppCompatActivity {
                 Log.e("Tag", "Place: " + place.getAddress() + place.getPhoneNumber());
 
                 ((TextView) findViewById(R.id.name))
-                        .setText("Name: "+place.getName());
+                        .setText("Name: " + place.getName());
 
                 ((TextView) findViewById(R.id.address))
-                        .setText("Address: "+place.getAddress());
+                        .setText("Address: " + place.getAddress());
 
                 ((TextView) findViewById(R.id.phoneno))
-                        .setText("Phone No.: "+place.getPhoneNumber());
+                        .setText("Phone No.: " + place.getPhoneNumber());
 
                 ((TextView) findViewById(R.id.rating))
-                        .setText("Rating: "+String.valueOf(place.getRating()));
+                        .setText("Rating: " + String.valueOf(place.getRating()));
 
                 ((TextView) findViewById(R.id.website))
-                        .setText("Website: "+String.valueOf(place.getWebsiteUri()));
+                        .setText("Website: " + String.valueOf(place.getWebsiteUri()));
 
 
             } else if (resultCode == PlaceAutocomplete.RESULT_ERROR) {
@@ -69,6 +135,32 @@ public class MainActivity extends AppCompatActivity {
             }
 
 
+
+
         }
+    }
+
+    public void onLocationChanged(Location location) {
+
+        TextView tvLocation = (TextView) findViewById(R.id.tv_location);
+
+        // Getting latitude of the current location
+        double latitude = location.getLatitude();
+
+        // Getting longitude of the current location
+        double longitude = location.getLongitude();
+
+        // Creating a LatLng object for the current location
+        LatLng latLng = new LatLng(latitude, longitude);
+
+        // Showing the current location in Google Map
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+
+        // Zoom in the Google Map
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+
+        // Setting latitude and longitude in the TextView tv_location
+        tvLocation.setText("Latitude:" +  latitude  + ", Longitude:"+ longitude );
+
     }
 }
